80aeb3cc199e409d6c2bd63122561ca11f350980
robotzulurain <robotzulurain@gmail.com>
2025-10-09 21:16:20 +0200

    Add superuser migration

---
+++ 0002_create_superuser.py
@@ -0,0 +1,23 @@
+from django.db import migrations
+from django.contrib.auth import get_user_model
+
+def create_superuser(apps, schema_editor):
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='admin',
+            email='robotzulurain@gmail.com',
+            password='admin123'
+        )
+        print("Superuser created successfully!")
+    else:
+        print("Superuser already exists!")
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('amr_reports', '0001_initial'),
+    ]
+    
+    operations = [
+        migrations.RunPython(create_superuser),
+    ]
